3c4ed4bc9a102a8c68ddfee6a740b5e9aabd54fc
amandeepdhillon123 <amandeep24397@gmail.com>
2023-09-27 11:06:55 +0530

    some change

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(cors());
 
 app.use(router);
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", (req, resp) => {
-  return  resp.sendFile(path.join(__dirname, "../client/build/index.html"));
+  return  resp.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 dbConnect();
